0f6ca7f60f2e786094ac4a278f3c695936777b84
Yaresko Kostya <zhtcmrj@gmail.com>
2018-12-01 20:58:53 +0200

    commit 2

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,35 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+    <!-- Main viev CSS -->
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/styles.css') }}">
+    <!-- Bootstrap 4 DatePicker CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/gijgo@1.9.10/css/gijgo.min.css" rel="stylesheet" type="text/css" />
+
+    <title>Registration Form</title>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+
+    <!-- Phone Mask JS -->
+    <script src="https://unpkg.com/imask"></script>
+
+    <!-- Bootstrap 4 DatePicker JS -->
+    <script src="https://cdn.jsdelivr.net/npm/gijgo@1.9.10/js/gijgo.min.js" type="text/javascript"></script>
+</head>
+<body>
+
+@yield('content')
+
+</body>
+</html>
---
+++ resources/views/members/index.blade.php
@@ -0,0 +1,59 @@
+@extends('layout')
+
+@section('content')
+
+    <div class="map">
+        <iframe width="100%" height="450" src="https://maps.google.com/maps?width=100%&amp;height=450&amp;hl=en&amp;q=7060%20Hollywood%20Blvd%2C%20Los%20Angeles%2C%20CA+(%D0%9D%D0%B0%D0%B7%D0%B2%D0%B0%D0%BD%D0%B8%D0%B5)&amp;ie=UTF8&amp;t=&amp;z=10&amp;iwloc=B&amp;output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>
+    </div>
+    <div class="container contact-form" align="center">
+        <h2 class="pb-4">To participate in the conference, please fill out the form:</h2>
+        @include('errors')
+        <p class="statusMsg"></p>
+        <div class="tab">
+            <form method="post" id="form1">
+                <p><input class="form-control" type="text" name="firstname" pattern="[A-Za-z]{1,32}" placeholder="first name*" required></p>
+                <p><input class="form-control" type="text" name="lastname" pattern="[A-Za-z]{1,32}" placeholder="last name*" required></p>
+                <p><input class="form-control" name="birthdate" id="datepicker" minlength="10" placeholder="birthdate*" required></p>
+                <p><input class="form-control" type="text" name="subject" pattern="[A-Za-z ]{1,80}" placeholder="report subject*" required></p>
+                @include('members.country')
+                <p><input class="form-control" type="text" name="phone" id="phone" minlength="15" placeholder="phone*" required></p>
+                <p><input class="form-control" type="email" name="email" placeholder="email*" required></p>
+                <p align="right"><input type="submit" name="btnSubmit" class="btnContact" id="subForm1" value="Next"/></p>
+            </form>
+        </div>
+
+        <div class="tab">
+            <form enctype="multipart/form-data" method="post" id="form2">
+                <p><input class="form-control" type="text" name="company" placeholder="company*" required></p>
+                <p><input class="form-control" type="text" name="position" placeholder="position*" required></p>
+                <p><textarea class="form-control" rows="8" name="about" placeholder="about me*" required></textarea></p>
+                <div class="input-group mb-3">
+                    <div class="custom-file">
+                        <label class="custom-file-label" for="inputPhoto">choose a photo</label>
+                        <input type="file" accept="image/*" class="custom-file-input" name="photo" id="inputPhoto" style="cursor: pointer">
+                    </div>
+                </div>
+                <p align="right"><input type="submit" name="btnSubmit" class="btnContact" id="subForm2" value="Next"/></p>
+            </form>
+        </div>
+
+        <div class="tab">
+            <h4>Share the link on social networks</h4>
+            <input type="button" class="btn btn-lg btn-fb" onclick="location.href='http://facebook.com/share?url=<?=$GLOBALS['url']?>'" value="Facebook"/>
+            <input type="button" class="btn btn-lg btn-tw" onclick="location.href='http://twitter.com/share?text=<?=$GLOBALS['message']?>&url=<?=$GLOBALS['url']?>&hashtags=conference,AngularJS';" value="Twitter"/>
+            <input type="button" class="btn btn-lg btn-gp" onclick="location.href='http://plus.google.com/share?url=<?=$GLOBALS['url']?>';" value="Google+"/>
+        </div>
+
+        <!-- Circles which indicates the steps of the form: -->
+        <div>
+            <span class="step"></span>
+            <span class="step"></span>
+            <span class="step"></span>
+        </div>
+
+        <h2 class="mt-4"><a href="/list">All members (<?=$data->fetchColumn()?>)</a></h2>
+
+    </div>
+    <script src="{{ asset('js/script.js') }}"></script>
+
+@endsection
